b710acc7d841f8aa56a5015997a5b667880e32c0
Thomas van der Veen <veent@uk.ibm.com>
2016-03-10 21:18:40 +0000

    Brought everything upto the latest levels.

--- project.clj
+++ project.clj
@@ -3,24 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jarohen/chord "0.4.2" :exclusions [org.clojure/clojure]]
-                 [ring/ring-core "1.2.0"]
-                 [compojure "1.1.5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jarohen/chord "0.7.0" :exclusions [org.clojure/clojure]]
+                 [ring/ring-core "1.4.0"]
+                 [compojure "1.5.0"]
                  [hiccup "1.0.5"]
-                 [org.clojure/core.async "0.1.301.0-deb34a-alpha"]
-                 [org.clojure/clojurescript "0.0-2411"]
-                 [jayq "2.5.2"]]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [jayq "2.5.4"]]
   :main ^:skip-aot flot.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
+  :profiles {:uberjar {:aot :all}
+             :dependencies [[javax.servlet/servlet-api "2.5"]]}
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.3"]]
 
   :cljsbuild {
-    :builds [{:source-paths ["src-cljs"]
-              :compiler {
-                :output-to "resources/public/js/flot.js"
-                ;;:output-dir ""
-               :optimizations :whitespace
-                :pretty-print true}}]})
+              :builds [{:source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "resources/public/js/flot.js"
+                                   :optimizations :advanced
+                                   :pretty-print true}}]})
